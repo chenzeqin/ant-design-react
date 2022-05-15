59c62c89683d6afa701477f7b39005fb433a4c78
chenzq <sailor_chenzeqin@163.com>
2022-05-16 00:47:36 +0800

    update lib name

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 library.add(fas)
-import React from 'react'
 
 export { default as Button } from './components/Button'
 export { default as Menu } from './components/Menu'
